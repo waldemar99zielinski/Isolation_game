ffe5616481fe75d659843ed4e18c48652581e9e5
KrissKry <edmaister02@gmail.com>
2020-11-25 20:28:24 +0100

    ai test

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 from board import Board
-from iso import get_all_possibilities, game
+from iso import get_all_possibilities, game, gameAI
 import constants
 
 class EndStateChecker(unittest.TestCase):
@@ -49,11 +49,12 @@ class EndStateChecker(unittest.TestCase):
                 2 ]
         '''
         first_player = 'u'
+        depth = 2
         board = Board(1, 2)
         board.set_position(0, 0, constants.PLAYER_1)
         board.set_position(0, 1, constants.PLAYER_2)
         # board.printArray()
-        result = game(board, first_player)
+        result = game(board, first_player, depth)
         self.assertEqual(
             first_player,
             result
@@ -64,13 +65,29 @@ class EndStateChecker(unittest.TestCase):
                 2 ]
         '''
         first_player = 'c'
+        depth = 2
         board = Board(1, 2)
         board.set_position(0, 0, constants.PLAYER_1)
         board.set_position(0, 1, constants.PLAYER_2)
         # board.printArray()
-        result = game(board, first_player)
+        result = game(board, first_player, depth)
         self.assertEqual(
             first_player,
+            result
+        )
+    def testAIvsAI(self):
+        '''[ 1 0 0
+             0 0 0
+             0 0 2]
+        '''
+        first_player = constants.PLAYER_1
+        depth = 2
+        board = Board(3,3)
+        board.set_position(0, 0, constants.PLAYER_1)
+        board.set_position(2, 2, constants.PLAYER_2)
+        result = gameAI(board, first_player, depth)
+        self.assertEqual(
+            'c',
             result
         )
 
